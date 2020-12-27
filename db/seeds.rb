ae58428e63188e492191f8beb8dab224fb93b111
miShelbyT <mi.shelbyrose@gmail.com>
2020-12-27 11:20:03 -0500

    added initial data in seeds file but haven't seeded yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+api_response = RestClient.get("https://rws-cards-api.herokuapp.com/api/v1/cards/")
+
+api_data = JSON.parse(api_response)
+
+api_data["cards"].each do |card|
+	type = card["type"]
+	name = card["name"]
+	meaning = card["meaning-up"]
+	meaning_rev = card["meaning_rev"]
+	desc = card["desc"]
+	Recipe.create(type: type, name: name, meaning: meaning, meaning_rev: meaning_rev, desc: desc)
+end
