04fcc91428579a27989145b9c9ec9e34f3621fb3
miShelbyT <mi.shelbyrose@gmail.com>
2020-12-30 13:07:33 -0500

    working on serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,14 +11,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    User.create(user_params)
+    user = User.create(user_params)
     render json: user
   end
 
   private
 
   def user_params
-    params(:user).permit(:user_name, :password)
+    params.permit(:user_name, :password)
   end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :cards, only: [:show, :index]
       resources :users, only: [:show, :index, :create]
+      post '/users/login', to: 'users#login'
     end
   end
 
