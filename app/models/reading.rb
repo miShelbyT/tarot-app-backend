71bf8451483e6ea567619d5b1b799b4cae2515fb
PatriciaArnedo <paaarnedo@gmail.com>
2021-01-03 15:19:56 -0500

    added dependent destroy to models

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,7 +1,7 @@
 class Reading < ApplicationRecord
   
   belongs_to :user
-  has_many :card_readings
+  has_many :card_readings, dependent: :destroy
   has_many :cards, through: :card_readings
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    has_many :readings
+    has_many :readings, dependent: :destroy
     validates :user_name, presence: true, uniqueness: true
     has_secure_password
 
